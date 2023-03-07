8766191fda217b6b3720d82e30969f26125c363a
Yuri Desideri <yuridesideri12@gmail.com>
2023-03-07 14:37:06 -0300

    feat: seeded the database with hotels and rooms

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { Hotel, PrismaClient } from '@prisma/client';
 import dayjs from 'dayjs';
 import { TicketType } from '@prisma/client';
 import { Prisma } from '@prisma/client';
@@ -6,7 +6,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   let event = await prisma.event.findFirst();
-  let ticketType : TicketType[] | Prisma.BatchPayload  = await prisma.ticketType.findMany();
+  let ticketType: TicketType[] | Prisma.BatchPayload = await prisma.ticketType.findMany();
+  let hotelsWithRooms: Hotel[] | Prisma.BatchPayload = await prisma.hotel.findMany();
 
   if (!event) {
     event = await prisma.event.create({
@@ -25,11 +26,61 @@ async function main() {
         { name: 'Online', price: 100, isRemote: true, includesHotel: false },
         { name: 'Presencial', price: 250, isRemote: false, includesHotel: false },
         { name: 'Presencial', price: 600, isRemote: false, includesHotel: true },
-      ]
+      ],
     });
   }
+  if (hotelsWithRooms.length !== 3) {
+    let hotel1WithRoom = await prisma.hotel.create({
+      data: {
+        name: 'Driven Resort',
+        image: 'https://pix10.agoda.net/hotelImages/124/1246280/1246280_16061017110043391702.jpg?ca=6&ce=1&s=1024x768',
+        Rooms: {
+          createMany: {
+            data: [
+              { name: 'Triple', capacity: 3 },
+              { name: 'Triple', capacity: 3 },
+            ],
+          },
+        },
+      },
+      
+    });
+    let hotel2WithRoom = await prisma.hotel.create({
+      data: {
+        name: 'Driven Resort',
+        image: 'https://thumbs.dreamstime.com/b/hotel-sign-16711677.jpg',
+        Rooms: {
+          createMany: {
+            data: [
+              { name: 'Single', capacity: 1 },
+              { name: 'Double', capacity: 2 },
+            ],
+          },
+        },
+      },
+      
+    });
+    let hotel3WithRoom = await prisma.hotel.create({
+      data: {
+        name: 'Driven Resort',
+        image: 'https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2022/04/12/1329/MUMGH-P0765-Inner-Courtyard-Hotel-Exterior-Evening.jpg/MUMGH-P0765-Inner-Courtyard-Hotel-Exterior-Evening.16x9.jpg',
+        Rooms: {
+          createMany: {
+            data: [
+              { name: 'Triple', capacity: 3 },
+              { name: 'Double', capacity: 2 },
+              { name: 'Single', capacity: 1},
+            ],
+          },
+        },
+      },
+      
+    });
 
-  console.log({ event, ticketType });
+    hotelsWithRooms = [hotel1WithRoom, hotel2WithRoom, hotel3WithRoom];
+  }
+
+  console.log({ event, ticketType, hotelsWithRooms });
 }
 
 main()
