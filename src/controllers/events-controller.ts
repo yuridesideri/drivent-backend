6526b9ec15151db48cb3170658ea32d6927da67d
Cintra <gui7lk@gmail.com>
2023-03-09 12:51:37 -0300

    fix: exclude console.log

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -5,7 +5,6 @@ import httpStatus from "http-status";
 export async function getDefaultEvent(_req: Request, res: Response) {
   try {
     const event = await eventsService.getFirstEvent();
-    console.log(event);
     return res.status(httpStatus.OK).send(event);
   } catch (error) {
     return res.status(httpStatus.NOT_FOUND).send({});
