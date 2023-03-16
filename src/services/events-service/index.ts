af66f949668b4b334993f4f628807b3fbf2f832c
Cintra <gui7lk@gmail.com>
2023-03-16 16:13:28 -0300

    fix: redis enviroment variable

--- src/services/events-service/index.ts
+++ src/services/events-service/index.ts
@@ -5,15 +5,15 @@ import { Event } from "@prisma/client";
 import dayjs from "dayjs";
 
 async function getFirstEvent(): Promise<GetFirstEventResult | string> {
-  const cacheKey = "event";
-
+  const cacheKey = `${process.env.REDIS_KEY}.event`;
+  
   const eventCache = await eventRepository.eventCache(cacheKey);
 
   if (!eventCache) {
     const event = await eventRepository.findFirst();
     if (!event) throw notFoundError();
 
-    await eventRepository.setEventCache(cacheKey, exclude(event, "createdAt", "updatedAt"))
+    await eventRepository.setEventCache(cacheKey, exclude(event, "createdAt", "updatedAt"));
 
     return exclude(event, "createdAt", "updatedAt");
   }
